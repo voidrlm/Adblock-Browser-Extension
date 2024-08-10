57cc8738fa148b200ed956412b92007de541261c
voidrlm <varjun609@gmail.com>
2024-08-11 01:59:48 +0530

    Changes with respect to blocking ads

--- background.js
+++ background.js
@@ -1,16 +1,9 @@
-const urls = [
-  "https://raw.githubusercontent.com/voidrlm/Adblock-Browser-Extension/main/custom_rules/voidrlm_block_list.txt",
-];
-
-async function fetchAndCombineRules() {
-  try {
-    const responses = await Promise.all(urls.map((url) => fetch(url)));
-    const texts = await Promise.all(
-      responses.map((response) => response.text())
-    );
-
-    const urlFilters = texts
-      .join("\n")
+fetch(
+  "https://raw.githubusercontent.com/voidrlm/Adblock-Browser-Extension/main/custom_rules/voidrlm_block_list.txt"
+)
+  .then((response) => response.text())
+  .then((text) => {
+    const urlFilters = text
       .split("\n")
       .filter((line) => line && !line.startsWith("!") && !line.startsWith("["))
       .map((line) => line.trim());
@@ -21,19 +14,11 @@ async function fetchAndCombineRules() {
       action: { type: "block" },
       condition: { urlFilter: filter },
     }));
+
+    chrome.declarativeNetRequest.updateDynamicRules({
+      addRules: rules,
+      removeRuleIds: rules.map((rule) => rule.id),
+    });
     console.log(rules);
-    chrome.declarativeNetRequest.updateDynamicRules(
-      {
-        addRules: rules,
-        removeRuleIds: [],
-      },
-      () => {
-        if (chrome.runtime.lastError) {
-          console.error(chrome.runtime.lastError.message);
-        }
-      }
-    );
-  } catch (error) {
-    console.error("Error fetching adblock lists:", error);
-  }
-}
+  })
+  .catch((error) => console.error("Error fetching adblock list:", error));
